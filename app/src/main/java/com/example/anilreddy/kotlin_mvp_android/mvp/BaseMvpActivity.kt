36155a0380f8a0e2ee5298ba28a06163db1fca32
AnilRdyAleti <AnilRdyAleti@github.com>
2018-06-26 16:05:37 -0400

    Created BaseMvpActivity

--- app/src/main/java/com/example/anilreddy/kotlin_mvp_android/mvp/BaseMvpActivity.kt
+++ app/src/main/java/com/example/anilreddy/kotlin_mvp_android/mvp/BaseMvpActivity.kt
@@ -1,2 +1,40 @@
 package com.example.anilreddy.kotlin_mvp_android.mvp
 
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+
+abstract class BaseMvpActivity<in V : BaseMvpView, T : BaseMvpPresenter<V>> : AppCompatActivity(), BaseMvpView {
+
+    protected abstract var mPresenter: T
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        mPresenter.attachView(this as V)
+    }
+
+    override fun getContext(): Context = this
+
+    override fun showError(error: String?) {
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showError(stringResId: Int) {
+        Toast.makeText(this, stringResId, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showMessage(message: String?) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showMessage(srtResId: Int) {
+        Toast.makeText(this, srtResId, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mPresenter.detachView()
+    }
+}
